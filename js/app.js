12c9ad0389dee1eb2b206307c898e62ccc72a727
Eva Zamudio <gabriela.osu@gmail.com>
2014-02-06 18:39:13 -0800

    add basic routes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 App = Ember.Application.create();
 
+  // App.Store = DS.Store.extend({
+  // this help us to be notify if the API breaks 
+  // revision: 12,
+  // it tell us where to look for those records that we ask for
+  // allow us to specify all our models in javascript and later on in the development cycle, we can switch to a live server
+  // adapter: 'DS.FixtureAdapter'
+// });
+
 App.Router.map(function() {
-  // put your routes here
+  // put your routes here that says when the users click on the specific tab, we want to render the correct info 
+  // Define all URLS 
+  // this.resource('post');
   this.resource('about');
+  this.resource('posts');
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
